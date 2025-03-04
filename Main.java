d5157e715fc9215de4e7492407b774bd5bac1dc6
Top102b <laamartiayman05@gmail.com>
2025-03-04 20:00:29 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+ * Clase principal de ejemplo que demuestra el uso de la clase refactorizada.
+ * @author Ayman
+ */
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
